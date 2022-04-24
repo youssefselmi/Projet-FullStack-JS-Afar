2834e50c6e78ed1ab2c690f49e852f72521df44e
iheb <iheb.sghairi@esprit.tn>
2022-04-24 09:21:26 +0200

    fonctionalite avance

--- Pi back/routes/items.js
+++ Pi back/routes/items.js
@@ -26,6 +26,7 @@ const storage = multer.diskStorage({
       try{
           
           const items = await Item.find()
+          console.log(items)
           res.json(items)
 
       }catch(err){
@@ -34,7 +35,7 @@ const storage = multer.diskStorage({
   })
   router.get('/categorie',async(req,res)=>{
     const categorie=req.query.categorie;
-    var condition=categorie ? { categorie: { $regex: new RegExp(categorie), $options: "i" } } : {};
+    var condition=categorie ? { categorie: { $regex: new RegExp(categorie), $options: "i" }  } : {};
     Item.find(condition)
     .then(data=>{
         res.send(data);
@@ -45,6 +46,66 @@ const storage = multer.diskStorage({
             err.message || "Some error occurred while retrieving tutorials."
         })
     })
+})
+router.get('/njareb',async(req,res)=>{
+    
+    
+
+   
+   
+
+  const items = await Item.find({favoris: true})
+
+ console.log(items)
+ var tab=new Array();
+ for(var i=0;i<items.length;i++)
+ {
+     tab[i]=items[i].categorie
+     console.log(tab[i])
+ }
+ var h1=tab[0];
+ var h2=tab[1];
+ var h3 = tab[2];
+ var h4=tab[3];
+ var h5=tab[4];
+ var h6=tab[6];
+ var h7=tab[7];
+ var h8=tab[8];
+ var h9=tab[9];
+ var h10=tab[10];
+
+Item.find({categorie: {$in :[h1,h2,h3,h4,h5,h6,h7,h8,h9]}})
+
+
+
+    .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+})
+router.get('/favoris',async(req,res)=>{
+    varMatch = { $match : { "favoris":true} };
+    varProject = { $project : {categorie:1, "_id":0}};
+    Item.aggregate( [ varMatch, varProject ] )
+    
+  // Item.find({favoris: true},{categorie:1,_id:0})
+
+    .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+
+
 })
   router.get('/:id',async(req,res)=>{
     try{
@@ -84,6 +145,7 @@ const storage = multer.diskStorage({
   router.put('/modifer/:id',async(req,res)=>{
       try{
           const item = await Item.findById(req.params.id)
+          console.log(item)
        //   item.itemName = req.body.itemName 
           item.price = req.body.price 
           
@@ -118,6 +180,7 @@ router.put('/modifier/:id/favoris',async(req,res)=>{
         res.send('Error'+ err)
     }
 })
+
   
 
 
