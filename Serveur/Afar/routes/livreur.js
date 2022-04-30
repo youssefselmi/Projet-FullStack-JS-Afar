affe96e5d67dc212635e264d5486ba88706feff7
alasoltani92 <79304747+alasoltani92@users.noreply.github.com>
2022-04-30 03:27:21 +0100

    livreur

--- Serveur/Afar/routes/livreur.js
+++ Serveur/Afar/routes/livreur.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var multer = require("multer");
 var router = express.Router();
+const ErrorResponse = require('../utils/errorResponse');
+
 
 
 const Livreur = require('../models/livreur');
@@ -8,27 +10,34 @@ const cors =require('cors');
 router.use(cors({
     origin: '*'
 }));
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${file.fieldname}_${+new Date()}.jpg`);
-  }
-});
-const upload = multer({
-  storage
-});
 
-router.get('/list',async(req,res)=>{
-  try{
-      const livreurs = await Livreur.find()
-      res.json(livreurs)
+router.get('/list',async(req,res,next)=>{
 
-  }catch(err){
-      res.send('Error' + err)
+
+  // enable pagination
+  const pageSize = 8;
+  const page = Number(req.query.pageNumber) || 1;
+  const count = await Livreur.find({}).estimatedDocumentCount();
+      
+    try {
+      const livreurs = await Livreur.find().sort({createdAt: -1}).populate("addr")
+      .skip(pageSize * (page-1))
+      .limit(pageSize)
+
+      res.status(200).json({
+          success: true,
+          livreurs,
+          page,
+          pages: Math.ceil(count / pageSize),
+          count
+      })
+      next();
+  } catch (error) {
+      res.status(500).json('Server error');
   }
 })
+   
+
 
 router.get('/:id',async(req,res)=>{
   try{
@@ -53,35 +62,31 @@ router.delete('/supprimer/:id',async(req,res)=>{
 });
 
   
-router.post('/add', upload.single("photo"),function(req, res, next) {
-    new Livreur({
+router.post('/add',async(req,res,next)=> {
+    const{
              
-    nom : req.body.nom,
-    prenom : req.body.prenom,
-    picture: req.body.picture,
-    email : req.body.email,
-    num : req.body.num,
-    addr:req.body.addr,
-    region: req.body.region,    
-    modele: req.body.modele,
-    type: req.body.type,
-    age: req.body.age,
-    disponibilite: req.body.disponibilite,
-    password:req.body.password,
+    nom , prenom , picture, email ,num , addr,region,modele,type,
+    age, disponibilite,
+    password }=req.body;
 
+   const livreurExists = await Livreur.findOne({ email });
 
-             })
-             .save(
-               (err, newlivreur)=>{
-    if (err) return res.json({succes:false,err});
-                     console.log("error message :" +err);
-                     return res.status(200).json({
-                         success:true,
-                     })
-     
-               }
-             );
-            })
+ 
+  if (livreurExists){
+    res.status(422).json("exist");
+  }
+  try{
+  const livreur = await Livreur.create(req.body);
+  res.status(201).json({
+    success: true,
+    livreur
+});
+  }
+  catch (err) {
+    next(err);
+  }
+})
+        
             router.patch("/updatelivreur/:id", async(req, res) => {
               try {
                   const { id } = req.params;
@@ -97,6 +102,8 @@ router.post('/add', upload.single("photo"),function(req, res, next) {
                   res.status(422).json(error);
               }
           })
+          
+      
 
 
 module.exports = router;
