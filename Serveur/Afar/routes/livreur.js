9eaaccfad3075b004b0f9acdb1a95c2496a2a236
alasoltani92 <79304747+alasoltani92@users.noreply.github.com>
2022-05-03 22:52:25 +0100

    soltani

--- Serveur/Afar/routes/livreur.js
+++ Serveur/Afar/routes/livreur.js
@@ -2,7 +2,7 @@ var express = require('express');
 var multer = require("multer");
 var router = express.Router();
 const ErrorResponse = require('../utils/errorResponse');
-
+const nodemailer = require("nodemailer");
 
 
 const Livreur = require('../models/livreur');
@@ -10,34 +10,15 @@ const cors =require('cors');
 router.use(cors({
     origin: '*'
 }));
+router.get('/list',async(req,res)=>{
+  try{
+      const livreurs = await Livreur.find()
+      res.json(livreurs)
 
-router.get('/list',async(req,res,next)=>{
-
-
-  // enable pagination
-  const pageSize = 8;
-  const page = Number(req.query.pageNumber) || 1;
-  const count = await Livreur.find({}).estimatedDocumentCount();
-      
-    try {
-      const livreurs = await Livreur.find().sort({createdAt: -1}).populate("addr")
-      .skip(pageSize * (page-1))
-      .limit(pageSize)
-
-      res.status(200).json({
-          success: true,
-          livreurs,
-          page,
-          pages: Math.ceil(count / pageSize),
-          count
-      })
-      next();
-  } catch (error) {
-      res.status(500).json('Server error');
+  }catch(err){
+      res.send('Error' + err)
   }
 })
-   
-
 
 router.get('/:id',async(req,res)=>{
   try{
@@ -103,6 +84,46 @@ router.post('/add',async(req,res,next)=> {
               }
           })
           
+router.post('/mail',async(req,res,next)=> {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const{ email,region }=req.body;
+    var maillist = [ await Livreur.findOne({ email })  ];
+    var livreurRegion = [ await Livreur.findOne({ region })  ];
+    //var userRegion= [ await User.findOne({ region })  ];
+    
+    var msg = {
+        from: "nsporify@gmail.com", //sender address
+        subject: "Demande livreur", //Subject line
+        text: "soltani ala 21136025  demande livreur", //plaintext body
+          
+        // html: "<b>Hello world ✔</b>" //html body
+    }
+    
+    
+    maillist.forEach(function (to, i , array) {
+      msg.to = to;
+    
+      transporter.sendMail(msg, function (err) {
+        if (err) { 
+          res.status(422).json("err");
+          return;
+        } else { 
+          res.status(201).json({
+            success: true,
+           
+        });
+        }
+    
+        if (i === maillist.length - 1) { msg.transport.close(); }
+      });
+    });
+  })
       
 
 
